559d0ee1aca297a74105fb604a50c7c601465ff0
Gabriela Fuentes <93365497+22Elgida@users.noreply.github.com>
2022-09-16 23:23:54 -0600

    Add files via upload

--- PagWeb/Apps/home/models.py
+++ PagWeb/Apps/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    creacion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.apellido)
+
+
+class Administrador(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    creacion = models.DateField(auto_now_add=True)
+    Estudiante = models.ManyToManyField(Estudiante)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.apellido)
